6f4875aa87225a6d0f29e301c1e97d072e8ecc72
Calvin Quach <calvinquach@gmail.com>
2017-05-14 22:34:52 -0400

    Add arrow for input. Add show function for memory snapshot

--- simulation.py
+++ simulation.py
@@ -12,23 +12,23 @@ num_printer = 0
 
 #get user input
 print("How much RAM Memory is there on the simulated computer?")
-mem_size = int(input())
+mem_size = int(input('>'))
 while(mem_size <= 0):
     print("Please print a valid size.")
-    mem_size = input()
+    mem_size = int(input('>'))
 
 print("How many hard disks the simulated computer has?")
-num_hdd = int(input())
+num_hdd = int(input('>'))
 while(num_hdd <= 0):
     print("Please print a valid size.")
-    mem_size = input()
+    mem_size = int(input('>'))
 
 
 print("How many printers the simulated computer has?")
-num_printer = int(input())
+num_printer = int(input('>'))
 while(num_printer <= 0):
     print("Please print a valid size.")
-    mem_size = input()
+    mem_size = int(input('>'))
 
 print('Your simulated computer has {} bytes of RAM,  {} HDD(s),  {} Printer(s).'.format(mem_size, num_hdd, num_printer))
 
@@ -45,23 +45,32 @@ user_input = ''
 #wait for input
 while(True):
     print("What would you like to do?")
-    user_input = input()
+    user_input = input('>')
     #verify inputs
     while(not utils.isValid(user_input)):
         print("Enter valid command!")
-        user_input = input()
+        user_input = input('>')
     #do command
     if(utils.new_process.match(user_input)):
         #parse the input, retrieve the priority and size, and create the new process
         variables = re.split('\W+',user_input)
-        #check if there is enough space in memory
         #create process and pcb
         p = Process(int(variables[1]), int(variables[2]))
-        suc = mem_manager.put(p)
-        print(suc)
-        #put it into cpu if 
+        success = mem_manager.put(p)
+        if(not success):
+            print('No room in memory.')
+            break
+
         if(sched.peek_highest() == -1 and not cpu.isBusy()):
+            #there are no schedule processes and cpu is free
             cpu.run(p)
+        elif(cpu.check_priority() < p.priority):
+            #prempt cpu and allow higher prioty process into cpu
+            print('Preempted')
+            temp = cpu.terminate()
+            cpu.run(p)
+            #put back into queue
+            sched.put(temp)
         else:
             print('Goes to the ready queue!')
             sched.put(p)
@@ -77,6 +86,7 @@ while(True):
     if(utils.print_complete.match(user_input)):
         pass
     if(utils.show_ready.match(user_input)):
+        mem_manager.snapshot()
         pass
     if(utils.show_io_ready.match(user_input)):
         pass
