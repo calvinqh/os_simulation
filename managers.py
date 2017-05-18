d26200bd912c6e3fd2f12cb30cebe58e0da44502
Calvin Quach <calvinquach@gmail.com>
2017-05-18 01:54:51 -0400

    Add p_id for processes.

--- managers.py
+++ managers.py
@@ -17,6 +17,7 @@ class MemoryManager():
                 if(process.size <= self.memory[i].size):
                     #insert at the front of the slot and reduce slot size
                     p = UsedMemorySlot(process)
+                    self.pcb_map[process.id]=process
                     self.memory[i].size = self.memory[i].size - p.size
                     if(self.memory[i].size == 0):
                         self.memory.pop(i)
@@ -26,14 +27,38 @@ class MemoryManager():
                     success = True
                     break
         return success
-    
+
+    def release(self, process):
+        for i in range(len(self.memory)):
+            if(not self.memory[i].free and process.id == self.memory[i].process.id):
+                #clear it
+                free_slot = FreeMemorySlot(process.size)
+                self.memory = self.memory[:i] + [free_slot] + self.memory[i+1:]
+                if(i-1>=0 and i+1<len(self.memory) and self.memory[i-1].free and self.memory[i+1].free):
+                    #merge left and right
+                    fs = FreeMemorySlot(process.size+self.memory[i-1].size+self.memory[i+1].size)
+                    self.memory = self.memory[:i-1] + [fs] + self.memory[i+2:]
+                elif(i-1>=0 and self.memory[i-1].free):
+                    #merge left
+                    fs = FreeMemorySlot(process.size+self.memory[i-1].size)
+                    self.memory = self.memory[:i-1] + [fs] + self.memory[i+1:]
+                elif(i+1<len(self.memory) and self.memory[i+1].free):
+                    #merge right
+                    fs = FreeMemorySlot(process.size+self.memory[i+1].size)
+                    self.memory = self.memory[:i] + [fs] + self.memory[i+2:]
+                break;
+        print("Process has been released.")
+
     def snapshot(self):
         start = 0
         counter = 0
         for i in range(len(self.memory)):
-            print('Process {}: {} {}'.format(counter,start,start+self.memory[i].size))
-            start += self.memory[i].size
-            counter+=1
+            if(self.memory[i].free):
+                print('Free Memory: {} {}'.format(start,start+self.memory[i].size))
+                start += self.memory[i].size
+            else:
+                print('Process {}: {} {}'.format(self.memory[i].process.id,start,start+self.memory[i].size))
+                start += self.memory[i].size
 
 
 class FreeMemorySlot():
@@ -48,13 +73,6 @@ class UsedMemorySlot(FreeMemorySlot):
         FreeMemorySlot.__init__(self,p.size)
         self.free = False
         self.process = p
-
-class PCB():
-
-    def __init__(self):
-        self.pid = 0
-        self.starting_addr = 0
-        self.state = None
 
 class HardDriveManager():
     
--- process.py
+++ process.py
@@ -1,5 +1,10 @@
 class Process: 
-    
+
+    p_id = 1
+
     def __init__(self, priority, size):
+        self.id = Process.p_id
+        Process.p_id += 1
         self.priority = priority
         self.size = size
+        
